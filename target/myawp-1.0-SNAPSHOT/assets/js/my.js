bc9068ee4e73b80b6bb729f136aeda3a35ecd9d1
rishabh9120 <rishabhagrawal9120@gmail.com>
2020-05-24 16:45:39 +0530

    initial commit

--- target/myawp-1.0-SNAPSHOT/assets/js/my.js
+++ target/myawp-1.0-SNAPSHOT/assets/js/my.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-	const number = $('[name="number"]').mask("(0000)-000-000")
-},
+// $(document).ready(function(){
+// 	const number = $('[name="number"]').mask("(0000)-000-000")
+// }
 function validateLogin() {
 	var email = $('[name = "email"]').val();
 	var pwd = $('[name = "password"]').val();
@@ -16,43 +16,43 @@ function validateLogin() {
 		retval = false;
 	}
 	return retval;
-},
-function validateRegister() {
-    let retval = true;
-	var fname = $('[name="first_name"]').val();
-	var lname = $('[name="last_name"]').val();
-	const email = $('[name="email"]').val();
-	const number = $('[name="number"]').val();
-	const pwd = $('[name="password"]').val();
-	const pwd2 = $('[name="password_repeat"]').val();
-	if (pwd != pwd2) {
-		$('[name="password_repeat"]').addClass('is-invalid');
-		retval = false;
-	}
-
-	if (!pwd.match('^(?=.*[w])(?=.*[W])[wW]{8,}$')) {
-		$('[name="password"]').addClass('is-invalid');
-		retval = false;
-	}
-	if (fname.length == 0) {
-		$('[name="first_name"]').addClass('is-invalid');
-		retval = false;
-	}
-	if (lname.length == 0) {
-		$('[name="last_name"]').addClass('is-invalid');
-		retval = false;
-	}
-	if (
-		!email.match(
-			'^[a-zA-Z0-9_+&*-] + (?:\\.[a-zA-Z0-9_+&*-]+ )*@(?:[a-zA-Z0-9-]+\\.) + [a-zA-Z]{2, 7}'
-		)
-	) {
-		$('[name="email"]').addClass('is-invalid');
-		retval = false;
-	}
-	if (!number.match('(0/91)?[7-9][0-9]{9}')) {
-		$('[name="number"]').addClass('is-invalid');
-		retval = false;
-	}
-	return retval;
 }
+// function validateRegister() {
+//     let retval = true;
+// 	const fname = $('[name="first_name"]').val();
+// 	const lname = $('[name="last_name"]').val();
+// 	const email = $('[name="email"]').val();
+// 	const number = $('[name="number"]').val();
+// 	const pwd = $('[name="password"]').val();
+// 	const pwd2 = $('[name="password_repeat"]').val();
+// 	if (pwd != pwd2) {
+// 		$('[name="password_repeat"]').addClass('is-invalid');
+// 		retval = false;
+// 	}
+
+// 	if (!pwd.match('^(?=.*[w])(?=.*[W])[wW]{8,}$')) {
+// 		$('[name="password"]').addClass('is-invalid');
+// 		retval = false;
+// 	}
+// 	if (fname.length == 0) {
+// 		$('[name="first_name"]').addClass('is-invalid');
+// 		retval = false;
+// 	}
+// 	if (lname.length == 0) {
+// 		$('[name="last_name"]').addClass('is-invalid');
+// 		retval = false;
+// 	}
+// 	if (
+// 		!email.match(
+// 			'^[a-zA-Z0-9_+&*-] + (?:\\.[a-zA-Z0-9_+&*-]+ )*@(?:[a-zA-Z0-9-]+\\.) + [a-zA-Z]{2, 7}'
+// 		)
+// 	) {
+// 		$('[name="email"]').addClass('is-invalid');
+// 		retval = false;
+// 	}
+// 	if (!number.match('(0/91)?[7-9][0-9]{9}')) {
+// 		$('[name="number"]').addClass('is-invalid');
+// 		retval = false;
+// 	}
+// 	return retval;
+// }
